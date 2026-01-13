f7b0b007f7262db2ea834e84fbb115af45f6ccfe
MartinGebske <martin.gebske@hotmail.de>
2026-01-13 16:36:12 +0100

    Prepared lap time

--- repurpose-cars/environment/debug/game_manager.gd
+++ repurpose-cars/environment/debug/game_manager.gd
@@ -3,7 +3,8 @@ class_name GameManager
 
 @export var player: VehicleBody3D
 
-
+var is_racing := false
+var lap_time := 0.0
 
 func _ready() -> void:
 	freeze_player()
@@ -13,6 +14,11 @@ func freeze_player() -> void:
 	player.freeze_mode = RigidBody3D.FREEZE_MODE_STATIC
 	player.freeze = true
 
+func start_ride() -> void:
+	player.freeze = false
+	is_racing = true
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("go"):
-		player.freeze = false
+		start_ride()
